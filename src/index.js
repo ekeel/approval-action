01e33a32523cd5428d253abd50a508e8139e20c0
ekeel <e@ekeel.net>
2023-11-29 23:27:42 -0700

    Added issue creation.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const Core = require('@actions/core');
+const Github = require('@actions/github');
+
+(async () => {
+    try {
+        const token = Core.getInput('token');
+        const minimumApprovals = Core.getInput('minimumApprovals');
+        const issueTitle = Core.getInput('issueTitle');
+        const issueBody = Core.getInput('issueBody');
+        const excludeInitiator = Core.getInput('excludeInitiator');
+
+        const approversInput = Core.getInput('approvers');
+        const issueLabelsInput = Core.getInput('issueLabels');
+        const approveWordsInput = Core.getInput('approveWords');
+        const rejectWordsInput = Core.getInput('rejectWords');
+
+        const approvers = approversInput.split(',').map(approver => approver.trim());
+        const issueLabels = issueLabelsInput.split(',').map(label => label.trim());
+        const approveWords = approveWordsInput.split(',').map(word => word.trim());
+        const rejectWords = rejectWordsInput.split(',').map(word => word.trim());
+
+        const repoContext = Github.context.repo;
+        const owner = repoContext.owner;
+        const repo = repoContext.repo;
+
+        const octokit = new Github.GitHub(token);
+
+        const opts = Object.fromEntries(Object.entries({
+            owner,
+            repo,
+            issueTitle,
+            body: issueBody,
+            labels: issueLabels && issueLabels.length > 0 ? issueLabels : undefined,
+            assignees: approvers
+        }).filter(([_, v]) => v !== undefined));
+
+        const issue = await octokit.rest.issues.create(opts);
+    } catch (error) {
+        Core.error(error);
+        Core.setFailed(error.message);
+    }
+})();
 
 try {
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
   
 } catch (error) {
-    core.setFailed(error.message);
+    Core.setFailed(error.message);
 }
